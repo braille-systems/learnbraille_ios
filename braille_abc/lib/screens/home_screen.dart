bf40293358af9dffae505ffe5e44951583d57901
Alyona Voinova <nena.2000@mail.ru>
2021-02-25 00:57:19 +0300

    Create own .dart files for screens

--- braille_abc/lib/screens/home_screen.dart
+++ braille_abc/lib/screens/home_screen.dart
@@ -1,14 +1,19 @@
 import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:braille_abc/components/bottom_bar_widget.dart';
+
+import 'package:braille_abc/screens/practice_screen.dart';
+import 'package:braille_abc/screens/settings_screen.dart';
+import 'package:braille_abc/screens/study_screen.dart';
+import 'package:braille_abc/screens/dictionary_screen.dart';
+
 import 'package:braille_abc/components/menu_button_widget.dart';
 import 'package:braille_abc/models/app_model.dart';
 import 'package:braille_abc/models/menu_button.dart';
 import 'package:braille_abc/shared/screen_params.dart';
 
-// Main Screen
+
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreen createState() => _HomeScreen();
@@ -16,20 +21,16 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreen extends State<HomeScreen> {
   List<Widget> _pages = [
-    MenuScreen(), // see the FrontPage class
+    MenuScreen(),
     StudyScreen(),
     PracticeScreen(),
     DictionaryScreen(),
-    SettingsScreen() // see the SettingsPage class
+    SettingsScreen(),
   ];
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
         tabBar: CupertinoTabBar(
-          //iconSize: 45,
-          /*type: BottomNavigationBarType.shifting,
-      selectedItemColor: CupertinoColors.activeBlue,
-      unselectedItemColor: CupertinoColors.systemGrey,*/
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               backgroundColor: CupertinoColors.lightBackgroundGray,
@@ -47,62 +48,10 @@ class _HomeScreen extends State<HomeScreen> {
         tabBuilder: (BuildContext context, index) {
           return _pages[index];
         });
-       /* tabBuilder: (context, index) {
-          return CupertinoTabView(
-            builder: (context) {
-              return CupertinoPageScaffold(child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20,),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: ScreenParams.height(6, context)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Меню",
-                          style: TextStyle(
-                              color: CupertinoColors.black,
-                              fontSize: 55,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        CupertinoButton(
-                          child: Icon(
-                            CupertinoIcons.question_circle,
-                            size: ScreenParams.height(5, context),
-                          ),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: ScreenParams.height(5, context)),
-                    SizedBox(
-                      height: ScreenParams.height(70, context),
-                      child: GridView.count(
-                        childAspectRatio: (50 / 65),
-                        mainAxisSpacing: ScreenParams.height(3, context),
-                        crossAxisSpacing: ScreenParams.height(2, context),
-                        crossAxisCount: 2,
-                        children: <Widget>[
-                          for (int i = 0; i < AppModel.menuButton.length; i++)
-                            MenuButtonWidget(
-                              menuButton: MenuButton(
-                                  name: AppModel.menuButton[i].name,
-                                  icon: AppModel.menuButton[i].icon),
-                            ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              );
-            }
-          );
-        }*/
+
   }
 }
-// Front Page
+
 class MenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -153,40 +102,12 @@ class MenuScreen extends StatelessWidget {
       ),
     ),
     );
-
-  }
-}
-class StudyScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Study'),
-    );
-  }
-}
-class PracticeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Practice'),
-    );
-  }
-}
-class DictionaryScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Dictionary'),
-    );
   }
 }
 
-// Settings Page
-class SettingsScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Settings'),
-    );
-  }
-}
+
+
+
+
+
+
