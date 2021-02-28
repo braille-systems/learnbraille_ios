8dbc6eb4f44b2a13b4a76c76f2580db402ac1989
Alyona Voinova <60846221+pikabol88@users.noreply.github.com>
2021-02-28 19:21:52 +0300

    BRAILLE-70 feature/screens transition (#11)
    * BRAILLE-70 Add bottom bar with transitions
    
    * Create own .dart files for screens
    
    * BRAILLE-70 Add transitions using menu buttons
    
    * BRAILLE-70 Fixed tab bar disappearance
    
    * BRAILLE-70 Try to fix problem with screen transitions
    
    На данный момент переключение между экранами реализовано двумя способами:
    1. Кнопки главного меню
    2. TabBar
    Отдельно каждая из реализаций работает, но при попытке комбинировать, переходы очень странно себя ведут
    
    * BRAILLE-70 Fix code alignment
    
    * BRAILLE-70 Delete extra comments

--- braille_abc/test/widget_test.dart
+++ braille_abc/test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:braille_abc/braille_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:braille_abc/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
