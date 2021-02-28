0c56fe2efe0d2a407dab323ac91cc810bceb9209
Danil Ovechkin <47829271+OvechkinDanil@users.noreply.github.com>
2021-02-28 15:58:48 +0300

    Remove unused package

--- braille_abc/lib/braille_app.dart
+++ braille_abc/lib/braille_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:braille_abc/screens/home_screen.dart';
-import 'package:braille_abc/screens/letter_screen.dart';
 
 class BrailleApp extends StatelessWidget {
   @override
