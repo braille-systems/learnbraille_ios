5db910caa3e97937cff7bcab466372d4de10728b
OvechkinDanil <danil190600@gmail.com>
2021-03-01 15:13:45 +0300

    BRAILLE-74 Button don't work

--- braille_abc/lib/screens/letter_screen.dart
+++ braille_abc/lib/screens/letter_screen.dart
@@ -4,9 +4,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:braille_abc/components/navigation_bar_widget.dart';
 import 'package:braille_abc/components/letter_widget.dart';
+import 'package:braille_abc/symbol/symbol.dart';
+import 'dart:math' as math;
 
 class LetterScreen extends StatefulWidget {
-  const LetterScreen(
+  LetterScreen(
       {Key key,
       @required this.titleSymbol,
       @required this.symbol})
@@ -21,14 +23,25 @@ class LetterScreen extends StatefulWidget {
 }
 
 class _LetterScreenState extends State<LetterScreen> {
+  TextDirection _dir = TextDirection.ltr;
+
+  @override
+  void initState() {
+    super.initState();
+    print("InitState");
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("Build");
+
+
     return CupertinoPageScaffold(
       navigationBar: NavigationBar(title: widget.str),
       child: Column(
         children: [
           SizedBox(
-            height: ScreenParams.height(10, context),
+            height: ScreenParams.height(5, context),
           ),
           Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -39,17 +52,48 @@ class _LetterScreenState extends State<LetterScreen> {
                   symbol: widget.symbol,
                 ),
               ]),
-          SizedBox(
-            height: ScreenParams.height(15, context),
-          ),
+          // SizedBox(
+          //   height: ScreenParams.height(15, context),
+          // ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              // SizedBox(
+              //   height: ScreenParams.height(10, context),
+              //   width: ScreenParams.width(25, context),
+              // ),
+              Symbol(dir: _dir, char: widget.symbol, tap: false, Width: 200, Height: 350, location_y: 0.5),
               SizedBox(
                 height: ScreenParams.height(10, context),
                 width: ScreenParams.width(10, context),
               ),
-              ChangeModeWidget(),
+
+              // change_mode_widget(черная кнопка) я пока вынес сюда
+              Container(
+                  width: ScreenParams.width(40, context),
+                  transform: Matrix4.rotationZ(math.pi / 2),
+                  child: CupertinoButton(
+                      child: Icon(
+                        CupertinoIcons.arrow_up_arrow_down,
+                        color: CupertinoColors.white,
+                        size: 35,
+                        semanticLabel: "Изменить режим",
+                      ),
+                      disabledColor: CupertinoColors.black,
+                      color: CupertinoColors.black,
+                      onPressed: () => setState(()
+                        {
+                          if (this._dir == TextDirection.ltr)
+                              this._dir = TextDirection.rtl;
+                          else if (this._dir == TextDirection.rtl)
+                              this._dir = TextDirection.ltr;
+                        }
+                      )
+                  )
+              ),
             ],
           )
         ],
