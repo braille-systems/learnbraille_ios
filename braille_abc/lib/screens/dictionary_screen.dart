ad0dfe4dd9a7979870390082e575cc5687950ad1
Alyona Voinova <nena.2000@mail.ru>
2021-03-04 23:39:09 +0300

    BRAILLE-138 Add expansion list structure

--- braille_abc/lib/screens/dictionary_screen.dart
+++ braille_abc/lib/screens/dictionary_screen.dart
@@ -1,6 +1,7 @@
 import 'package:braille_abc/components/bottom_bar_widget.dart';
 import 'package:braille_abc/components/study_item_widget.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class DictionaryScreen extends StatelessWidget {
   @override
@@ -18,15 +19,91 @@ class DictionaryScreen extends StatelessWidget {
         ),
         trailing: Icon(CupertinoIcons.question_circle),
       ),
-      child: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          StudyItem(str: "А: точки 1"),
-          StudyItem(str: "Б: точки 1, 2"),
-          StudyItem(str: "В: точки 2, 4, 5, 6"),
-        ],
-      )),
+      child: SingleChildScrollView(
+          child:
+              MyExpanableCardViewFlutter()), //Wrapped into SingleChildScrollView because when click on TextField  keyboard will open and you may get error on screen "bottom overflowed by pixels flutter"
     );
   }
 }
+
+class MyExpanableCardViewFlutter extends StatefulWidget {
+  @override
+  _MyExpanableCardViewFlutterState createState() =>
+      _MyExpanableCardViewFlutterState();
+}
+
+class _MyExpanableCardViewFlutterState
+    extends State<MyExpanableCardViewFlutter> {
+  //controller for TextField
+  final username_controller = TextEditingController();
+  final password_controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            SizedBox(
+              width: 0,
+              height: 65,
+            ),
+            ExpansionTile(
+              leading: Icon(CupertinoIcons.textformat),
+              title: Text("Русский алфавит"),
+              children: <Widget>[
+                SafeArea(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StudyItem(str: "А: точки 1"),
+                        StudyItem(str: "Б: точки 1, 2"),
+                        StudyItem(str: "В: точки 2, 4, 5, 6"),
+                      ],
+                    )),
+              ],
+            ),
+            ExpansionTile(
+              leading: Icon(CupertinoIcons.number),
+              title: Text("Английский алфавит"),
+              children: <Widget>[
+                Text("................................................"),
+              ],
+            ),
+            ExpansionTile(
+              leading: Icon(CupertinoIcons.textformat),
+              title: Text("Цифры"),
+              children: <Widget>[
+
+              ],
+            ),
+            ExpansionTile(
+              leading: Icon(CupertinoIcons.textformat),
+              title: Text("Знаки препинания"),
+              children: <Widget>[
+                Text("................................................"),
+              ],
+            ),
+            ExpansionTile(
+              leading: Icon(CupertinoIcons.textformat),
+              title: Text("Арифметические знаки"),
+              children: <Widget>[
+                Text("................................................"),
+              ],
+            ),
+            ExpansionTile(
+              leading: Icon(CupertinoIcons.textformat),
+              title: Text("Признаки"),
+              children: <Widget>[
+                Text("................................................"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
