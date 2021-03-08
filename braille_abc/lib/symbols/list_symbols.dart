8772a334b036df9db1a648b0d5aab32baff3ecdb
ThinkingFrog <khripunkovdv@gmail.com>
2021-03-08 18:43:42 +0300

    Fix remaining flutter analyze warnings

--- braille_abc/lib/symbols/list_symbols.dart
+++ braille_abc/lib/symbols/list_symbols.dart
@@ -1,24 +1,24 @@
 import 'struct_symbol.dart';
 
-List<Struct_Symbol> list_Symbols = <Struct_Symbol>[
-  Struct_Symbol(list: Search.Image_Symbol(d: <int> []), char: null),
-  Struct_Symbol(list: Search.Image_Symbol(d: <int> [1]), char: 'А'),
-  Struct_Symbol(list: Search.Image_Symbol(d: <int> [1, 2]), char: 'Б'),
-  Struct_Symbol(list: Search.Image_Symbol(d: <int>[2, 4, 5, 6]), char: 'В'),
+List<StructSymbol> listSymbols = <StructSymbol>[
+  StructSymbol(list: Search.imageSymbol(d: <int> []), char: null),
+  StructSymbol(list: Search.imageSymbol(d: <int> [1]), char: 'А'),
+  StructSymbol(list: Search.imageSymbol(d: <int> [1, 2]), char: 'Б'),
+  StructSymbol(list: Search.imageSymbol(d: <int>[2, 4, 5, 6]), char: 'В'),
 ];
 
 class Search {
-  static Struct_Symbol Element(String ch){
+  static StructSymbol element(String ch){
     if(ch == null)
-      return list_Symbols[0];
-    for(int i = 1; i < list_Symbols.length; i++){
-      if(list_Symbols[i].char.compareTo(ch) == 0){
-        return list_Symbols[i];
+      return listSymbols[0];
+    for(int i = 1; i < listSymbols.length; i++){
+      if(listSymbols[i].char.compareTo(ch) == 0){
+        return listSymbols[i];
       }
     }
-    return list_Symbols[0];
+    return listSymbols[0];
   }
-  static List<bool> Image_Symbol({List<int> d}) {
+  static List<bool> imageSymbol({List<int> d}) {
     List<bool> data = <bool>[false, false, false, false, false, false];
     for (int i = 0; i < d.length; i++) {
       data[d[i] - 1] = true;
