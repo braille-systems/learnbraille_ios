096bf70c77f5f5c70f2fcff32da0bc02f7e807fc
Alyona Voinova <60846221+pikabol88@users.noreply.github.com>
2021-03-09 11:55:59 +0300

    Restyle menu buttons (#29)

--- braille_abc/lib/components/menu_button_widget.dart
+++ braille_abc/lib/components/menu_button_widget.dart
@@ -1,8 +1,10 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:braille_abc/screens/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:braille_abc/models/menu_button.dart';
 import 'package:braille_abc/shared/screen_params.dart';
+import 'package:decorated_icon/decorated_icon.dart';
 
 import 'bottom_bar_widget.dart';
 
@@ -34,11 +36,19 @@ class _MenuButtonWidget extends State<MenuButtonWidget> {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         primary: Colors.orange[300],
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25.0),
-          ),
-
-        textStyle: TextStyle(color:  Colors.white),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        textStyle: TextStyle(
+          color: Colors.white,
+          shadows: <Shadow>[
+            Shadow(
+              offset: Offset(3.0, 3.0),
+              blurRadius: 3.0,
+              color: CupertinoColors.separator,
+            ),
+          ],
+        ),
         padding: EdgeInsets.symmetric(vertical: 20),
       ),
       onPressed: () {
@@ -47,19 +57,27 @@ class _MenuButtonWidget extends State<MenuButtonWidget> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text(
+          AutoSizeText(
             widget.menuButton.name,
             style: TextStyle(
               fontSize: 30,
             ),
+            maxLines: 1,
           ),
           SizedBox(
             height: ScreenParams.height(4, context),
           ),
-          Icon(
+          DecoratedIcon(
             widget.menuButton.icon,
             color: Colors.white,
             size: 100,
+            shadows: <Shadow>[
+              Shadow(
+                offset: Offset(3.0, 3.0),
+                blurRadius: 4.0,
+                color: CupertinoColors.separator,
+              ),
+            ],
           ),
         ],
       ),
