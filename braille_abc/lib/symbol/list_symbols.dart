fb00732035ccf4110b1abf376b5e109228bff415
Oleg595 <60892746+Oleg595@users.noreply.github.com>
2021-03-15 11:57:21 +0300

    Braille 107 fix/corrections (#56)
    * WIP_BRAILLE-107_fix/corrections
    
    * WIP_BRAILLE-107_fix/correct StructSymbol
    
    * WIP_BRAILLE-107_fix/corrections
    
    * BRAILLE-107_fix/code_corrections
    
    * BRAILLE-107_fix/add_symbols
    
    * BRAILLE-108_fix/add_numbers
    
    * BRAILLE-109_feature/add_signs
    
    * BRAILLE-107_fix/fix_immutable_class
    
    * BRAILLE-107_fix/correction_bugs_in_symbol
    
    * BRAILLE-107_fix/fix_study_item_widget

--- braille_abc/lib/symbol/list_symbols.dart
+++ braille_abc/lib/symbol/list_symbols.dart
@@ -5,21 +5,51 @@ import 'struct_symbol.dart';
 class SymbolsFactory {
   SymbolsFactory();
 
-  List<StructSymbol> createSymbolsGroup(final String groupName) {
+  List<Symbol> createSymbolsGroup(final String groupName) {
     switch (groupName) {
       case "Русский алфавит":
         {
-          List<StructSymbol> russian = <StructSymbol>[
+          List<Symbol> russian = <Symbol>[
             RussianSymbol(list: Search.imageSymbol(d: <int>[1]), char: "А"),
             RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2]), char: "Б"),
             RussianSymbol(list: Search.imageSymbol(d: <int>[2, 4, 5, 6]), char: "В"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 4, 5]), char: "Г"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 4, 5]), char: "Д"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 5]), char: "Е"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 6]), char: "Ё"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[2, 4, 5]), char: "Ж"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 3, 5, 6]), char: "З"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[2, 4]), char: "И"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 3, 4, 6]), char: "Й"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 3]), char: "К"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 3]), char: "Л"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 3, 4]), char: "М"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 3, 4, 5]), char: "Н"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 3, 5]), char: "О"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 3, 4]), char: "П"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 3, 5]), char: "Р"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[2, 3, 4]), char: "С"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[2, 3, 4, 5]), char: "Т"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 3, 6]), char: "У"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 4]), char: "Ф"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 5]), char: "Х"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 4]), char: "Ц"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 3, 4, 5]), char: "Ч"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 5, 6]), char: "Ш"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 3, 4, 6]), char: "Щ"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 3, 5, 6]), char: "Ъ"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[2, 3, 4, 6]), char: "Ы"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[2, 3, 4, 5, 6]), char: "Ь"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[2, 4, 6]), char: "Э"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 5, 6]), char: "Ю"),
+            RussianSymbol(list: Search.imageSymbol(d: <int>[1, 2, 4, 6]), char: "Я"),
           ];
           return russian;
         }
         break;
       case "Знаки препинания":
         {
-          List<StructSymbol> punctuationMarks = <StructSymbol>[
+          List<Symbol> punctuationMarks = <Symbol>[
             PunctuationSymbol(list: Search.imageSymbol(d: <int>[2]), char: "Запятая"),
             PunctuationSymbol(list: Search.imageSymbol(d: <int>[3, 6]), char: "Дефис"),
             PunctuationSymbol(list: Search.imageSymbol(d: <int>[2, 5, 6]), char: "Точка"),
@@ -40,7 +70,7 @@ class SymbolsFactory {
 
       case "Арифметические знаки":
         {
-          List<StructSymbol> arithmeticSigns = <StructSymbol>[
+          List<Symbol> arithmeticSigns = <Symbol>[
             ArithmeticSymbol(list: Search.imageSymbol(d: <int>[2, 3, 5]), char: "Знак Плюс"),
             ArithmeticSymbol(list: Search.imageSymbol(d: <int>[3, 6]), char: "Знак Минус"),
             ArithmeticSymbol(list: Search.imageSymbol(d: <int>[3]), char: "Знак умножения точкой"),
@@ -52,6 +82,36 @@ class SymbolsFactory {
           return arithmeticSigns;
         }
         break;
+      case "Цифры":
+        {
+          List<Symbol> numbers = <Symbol>[
+            Number(list: Search.imageSymbol(d: <int>[1]), char: "1"),
+            Number(list: Search.imageSymbol(d: <int>[1, 2]), char: "2"),
+            Number(list: Search.imageSymbol(d: <int>[1, 4]), char: "3"),
+            Number(list: Search.imageSymbol(d: <int>[1, 4, 5]), char: "4"),
+            Number(list: Search.imageSymbol(d: <int>[1, 5]), char: "5"),
+            Number(list: Search.imageSymbol(d: <int>[1, 2, 4]), char: "6"),
+            Number(list: Search.imageSymbol(d: <int>[1, 2, 4, 5]), char: "7"),
+            Number(list: Search.imageSymbol(d: <int>[1, 2, 5]), char: "8"),
+            Number(list: Search.imageSymbol(d: <int>[2, 4]), char: "9"),
+            Number(list: Search.imageSymbol(d: <int>[2, 4, 5]), char: "0"),
+          ];
+          return numbers;
+        }
+        break;
+      case "Признаки":
+        {
+          List<Symbol> signs = <Symbol>[
+            Sign(list: Search.imageSymbol(d: <int>[3, 4, 5, 6]), char: "Цифровой знак"),
+            Sign(list: Search.imageSymbol(d: <int>[4, 5, 6]), char: "Признак курсивного шрифта"),
+            Sign(list: Search.imageSymbol(d: <int>[1, 2, 4, 5, 6]), char: "Признак жирного шрифта"),
+            Sign(list: Search.imageSymbol(d: <int>[4, 5]), char: "Признак большой русской буквы"),
+            Sign(list: Search.imageSymbol(d: <int>[6]), char: "Признак малой латинской буквы"),
+            Sign(list: Search.imageSymbol(d: <int>[4, 6]), char: "Признак большой латинской буквы"),
+          ];
+          return signs;
+        }
+        break;
     }
     return null;
   }
@@ -60,7 +120,13 @@ class SymbolsFactory {
 class Alphabet {
   Alphabet() {
     if (_alphabet == null) {
-      final List<String> tokens = <String>["Русский алфавит", "Знаки препинания", "Арифметические знаки"];
+      final List<String> tokens = <String>[
+        "Русский алфавит",
+        "Знаки препинания",
+        "Арифметические знаки",
+        "Цифры",
+        "Признаки",
+      ];
       SymbolsFactory factory = SymbolsFactory();
       _alphabet = Map();
       for (var token in tokens) {
@@ -69,7 +135,7 @@ class Alphabet {
     }
   }
 
-  List<StructSymbol> listOfSymbols(String key) {
+  List<Symbol> listOfSymbols(String key) {
     if (_alphabet.containsKey(key)) {
       return _alphabet[key];
     }
@@ -78,26 +144,23 @@ class Alphabet {
 
   List<StudyItem> listOfStudyItems(String key) {
     List<StudyItem> lsi = [];
-    List<StructSymbol> los = listOfSymbols(key);
+    List<Symbol> los = listOfSymbols(key);
     for (var symbol in los) {
       if (symbol.char != null) {
-        lsi.add(StudyItem(str: symbol.stringPoints(), titleSymbol: symbol.ofGroup()));
+        lsi.add(StudyItem(
+            symbol: symbol, titleSymbol: symbol.ofGroup()));
       }
     }
     return lsi;
   }
 
-  static Map<String, List<StructSymbol>> _alphabet;
+  static Map<String, List<Symbol>> _alphabet;
 }
 
 class Search {
-  // ignore: non_constant_identifier_names
-  static StructSymbol element(String ch, String keymap) {
-    if (ch == null) {
-      return null;
-    }
+  static Symbol element(String ch, String keymap) {
     Alphabet alphabet = Alphabet();
-    List<StructSymbol> list = alphabet.listOfSymbols(keymap);
+    List<Symbol> list = alphabet.listOfSymbols(keymap);
 
     for (int i = 0; i < list.length; i++) {
       if (list[i].char.compareTo(ch) == 0) {
@@ -107,11 +170,10 @@ class Search {
     return null;
   }
 
-  // ignore: non_constant_identifier_names
-  static List<bool> imageSymbol({List<int> d}) {
-    List<bool> data = <bool>[false, false, false, false, false, false];
+  static List<DotImage> imageSymbol({List<int> d}) {
+    List<DotImage> data = <DotImage>[DotImage(1, false), DotImage(2, false), DotImage(3, false), DotImage(4, false), DotImage(5, false), DotImage(6, false)];
     for (int i = 0; i < d.length; i++) {
-      data[d[i] - 1] = true;
+      data[d[i] - 1].setIsPressed(true);
     }
     return data;
   }
