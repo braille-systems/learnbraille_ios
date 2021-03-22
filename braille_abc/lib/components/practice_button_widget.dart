a4fb151a55417a772a4eb1c5a77c1917432aadd6
Kuksenko <60854701+kirillkuks@users.noreply.github.com>
2021-03-22 23:30:31 +0300

    BRAILLE-167 Fix dart analysis warning

--- braille_abc/lib/components/practice_button_widget.dart
+++ braille_abc/lib/components/practice_button_widget.dart
@@ -1,5 +1,4 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:braille_abc/screens/letter_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:braille_abc/shared/screen_params.dart';
