a4abde691e3bb594e6851e804277f59c1e0ea6e8
Kuksenko <60854701+kirillkuks@users.noreply.github.com>
2021-03-22 23:20:05 +0300

    BRAILLE-167 Add list in practice screen

---
+++ braille_abc/lib/components/practice_button_widget.dart
@@ -0,0 +1,163 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:braille_abc/screens/letter_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:braille_abc/shared/screen_params.dart';
+import 'package:decorated_icon/decorated_icon.dart';
+import 'package:braille_abc/screens/practice_screen.dart';
+
+import 'package:braille_abc/style.dart';
+
+import '../models/menu_button.dart';
+
+List<String> pool = [];
+
+class ContinueButtonWidget extends StatefulWidget {
+  @override
+  State<ContinueButtonWidget> createState() => _ContinueButtonWidget();
+}
+
+class _ContinueButtonWidget extends State<ContinueButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      label: "Продолжить",
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.orange[300],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            AutoSizeText(
+              "Продолжить",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w300, color: CupertinoColors.black),
+            ),
+            SizedBox(
+              width: ScreenParams.width(25, context),
+            ),
+            DecoratedIcon(
+              CupertinoIcons.chevron_right_2,
+              color: CupertinoColors.black,
+              size: 22.0,
+            ),
+          ],
+        ),
+        onPressed: () {
+          if (Practice.getPool().isNotEmpty) {
+            print(Practice.getPool());
+          }
+        },
+      ),
+    );
+  }
+}
+
+class PracticeButtonWidget extends StatefulWidget {
+  const PracticeButtonWidget({
+    Key key,
+    @required this.practiceButton,
+  }) : super(key: key);
+
+  final MenuButton practiceButton;
+
+  @override
+  State<PracticeButtonWidget> createState() => _PracticeButtonWidget();
+}
+
+class _PracticeButtonWidget extends State<PracticeButtonWidget> {
+  bool checkBox = false;
+
+  void onChanged(bool val) {
+    setState(
+      () {
+        checkBox = val;
+        if (checkBox) {
+          Practice.addSymbolGroup(widget.practiceButton.name);
+          //pool.add(widget.practiceButton.name);
+        } else {
+          Practice.removeSymbolGroup(widget.practiceButton.name);
+          //pool.remove(widget.practiceButton.name);
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      label: widget.practiceButton.name,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.orange[300],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          textStyle: TextStyle(
+            color: CupertinoColors.white,
+            shadows: <Shadow>[
+              Styles.buildButtonShadow(),
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 50,
+              child: AutoSizeText(
+                widget.practiceButton.name,
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: ScreenParams.height(2.0, context),
+            ),
+            DecoratedIcon(
+              widget.practiceButton.icon,
+              color: CupertinoColors.white,
+              size: 50.0,
+              shadows: <Shadow>[
+                Styles.buildButtonShadow(),
+                Shadow(
+                  offset: Offset(-.25, -.25),
+                  color: Colors.black45,
+                ),
+                Shadow(
+                  offset: Offset(.25, -.25),
+                  color: Colors.black45,
+                ),
+                Shadow(
+                  offset: Offset(-.25, .25),
+                  color: Colors.black45,
+                ),
+                Shadow(
+                  offset: Offset(.25, .25),
+                  color: Colors.black45,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Checkbox(
+                  value: checkBox,
+                  onChanged: (bool val) {
+                    onChanged(val);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+        onPressed: () {
+          onChanged(!checkBox);
+        },
+      ),
+    );
+  }
+}
--- braille_abc/lib/screens/practice_screen.dart
+++ braille_abc/lib/screens/practice_screen.dart
@@ -1,6 +1,29 @@
 import 'package:braille_abc/components/navigation_bar_widget.dart';
+import 'package:braille_abc/components/practice_button_widget.dart';
+import 'package:braille_abc/models/app_model.dart';
 import 'package:braille_abc/models/screen_model.dart';
+import 'package:braille_abc/shared/screen_params.dart';
 import 'package:flutter/cupertino.dart';
+
+import '../models/menu_button.dart';
+
+//TODO: Replace this class to other. Mb remove static.
+class Practice {
+  static List<String> _pool = [];
+
+  static void addSymbolGroup(String groupName) {
+    _pool.add(groupName);
+  }
+  static void removeSymbolGroup(String groupName) {
+    _pool.remove(groupName);
+  }
+  static List<String> getPool() {
+    return _pool;
+  }
+  static void updatePool() {
+    _pool.clear();
+  }
+}
 
 class PracticeScreen extends NavigationScreen {
   const PracticeScreen({
@@ -17,7 +40,41 @@ class PracticeScreen extends NavigationScreen {
         title: "Практика",
       ),
       child: SafeArea(
-        child: Center(child: Text('practice')),
+        minimum: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: ScreenParams.height(4, context),
+            ),
+            Column(
+              children: [
+                Container(
+                  height: ScreenParams.heightIOS14(72, context),
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    physics: NeverScrollableScrollPhysics(),
+                    mainAxisSpacing: ScreenParams.width(4, context),
+                    crossAxisSpacing: ScreenParams.height(4, context),
+                    children: <Widget>[
+                      for (var i = 0; i < AppModel.practiceButtons.length; ++i)
+                        PracticeButtonWidget(
+                          practiceButton: MenuButton(
+                            name: AppModel.practiceButtons[i].name,
+                            icon: AppModel.practiceButtons[i].icon,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 48,
+                  child: ContinueButtonWidget(),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
