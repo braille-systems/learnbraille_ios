b6052770b494456f5e5de2732d795e6bdc142722
OvechkinDanil <danil190600@gmail.com>
2021-03-28 22:47:18 +0300

    BRAILLE-182 Fix bugs

--- braille_abc/lib/symbol/image_symbol.dart
+++ braille_abc/lib/symbol/image_symbol.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:braille_abc/models/app_names.dart';
+import 'package:braille_abc/style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -8,7 +9,6 @@ import 'package:braille_abc/symbol/struct_symbol.dart';
 import 'package:braille_abc/symbol/list_symbols.dart';
 import 'package:braille_abc/models/practice_model.dart';
 
-@immutable
 class SymbolWidget extends StatefulWidget {
   final double width;
   final double height;
@@ -17,13 +17,14 @@ class SymbolWidget extends StatefulWidget {
   final bool isTapped;
   final TextDirection Function() textDir;
 
-  SymbolWidget({Key key,
-    @required this.textDir,
-    @required this.char,
-    @required this.dictSection,
-    @required this.isTapped,
-    @required this.width,
-    @required this.height})
+  SymbolWidget(
+      {Key key,
+        @required this.textDir,
+        @required this.char,
+        @required this.dictSection,
+        @required this.isTapped,
+        @required this.width,
+        @required this.height})
       : super(key: key) {
     createState();
   }
@@ -37,7 +38,7 @@ class _SymbolState extends State<SymbolWidget> {
   final bool isTapped;
 
   _SymbolState({String char, SectionType dictSection, @required this.isTapped}) {
-    if (!isTapped) {
+    if(!isTapped) {
       symbol = Search.element(char, dictSection);
     }
     else {
@@ -48,60 +49,14 @@ class _SymbolState extends State<SymbolWidget> {
   @override
   Widget build(BuildContext context) {
     return Stack(
-        textDirection: widget.textDir(),
-        children: <Widget>[
-          Container(
-            height: widget.height,
-            width: widget.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              color: AppColors.first,
-            ),
-            child: Wrap(
-              alignment: WrapAlignment.center,
-              runAlignment: WrapAlignment.center,
-              textDirection: widget.textDir(),
-              spacing: 55.0 / 667 * widget.height,
-              direction: Axis.vertical,
-              runSpacing: 40.0 / 667 * widget.height,
-              children: symbol.dots
-                  .map((item) => Semantics(
-                        label: SemanticNames.getName(SemanticsType.Dot) +
-                            item.outputData +
-                            (item.press
-                                ? SemanticNames.getName(SemanticsType.Painted)
-                                : SemanticNames.getName(SemanticsType.NotPainted)),
-                        button: false,
-                       child: Container(
-                         height: 75.0 / 330 * widget.height,//all proportions are relative to height and width of widget
-                         width: 75.0 / 330 * widget.height,//proportions are made as on the layout: https://www.figma.com/file/pJE5TUjBKvdy2ZmpMnHAS4/Практика
-                         decoration: BoxDecoration(
-                           shape: BoxShape.circle,
-                           border: Border.all(
-                             color: AppColors.dotBoarder,
-                             width: 8,
-                           ),
-                         ),
-                         child: CupertinoButton(
-                            onPressed: () {
-                              if (widget.isTapped) {
-                                setState(() {
-                                  item.setIsPressed(!item.press);
-                                  PracticeResults.dotClick(int.parse(item.outputData));
-                                });
-                              }
-                            },
-                            borderRadius: BorderRadius.all(Radius.circular(1000),),
-                            padding: EdgeInsets.zero,
-                            color: item.p,
-                            child: Text(item.outputData,
-                                textDirection: TextDirection.ltr,
-                                style: TextStyle(fontSize: 0.18 * widget.height - 8 / 330 * widget.height, color: item.onP, fontWeight: FontWeight.bold)),
-                          ),
-                        ),
-                      ))
-                  .toList(growable: false),
-            ),
+      textDirection: widget.textDir(),
+      children: <Widget>[
+        Container(
+          height: widget.height,
+          width: widget.width,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: AppColors.first,
           ),
           child: Wrap(
             alignment: WrapAlignment.center,
@@ -111,45 +66,41 @@ class _SymbolState extends State<SymbolWidget> {
             direction: Axis.vertical,
             runSpacing: 40.0 / 667 * widget.height,
             children: symbol.dots
-                .map((item) =>
-                Semantics(
-                  label: SemanticNames.getName(SemanticsType.Dot) +
-                      item.outputData +
-                      (item.press
-                          ? SemanticNames.getName(SemanticsType.Painted)
-                          : SemanticNames.getName(SemanticsType.NotPainted)),
-                  button: false,
-                  child: Container(
-                    //all proportions are relative to height and width of widget
-                    height: 75.0 / 330 * widget.height,
-                    //proportions are made as on the layout: https://www.figma.com/file/pJE5TUjBKvdy2ZmpMnHAS4/Практика
-                    width: 75.0 / 330 * widget.height,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: CupertinoColors.black,
-                        width: 8,
-                      ),
-                    ),
-                    child: CupertinoButton(
-                      onPressed: () {
-                        if (widget.isTapped) {
-                          setState(() {
-                            item.setIsPressed(!item.press);
-                          });
-                        }
-                      },
-                      borderRadius: const BorderRadius.all(Radius.circular(1000),),
-                      padding: EdgeInsets.zero,
-                      color: item.p,
-                      child: Text(item.outputData,
-                          textDirection: TextDirection.ltr,
-                          style: TextStyle(fontSize: 0.18 * widget.height - 8 / 330 * widget.height,
-                              color: item.onP,
-                              fontWeight: FontWeight.bold)),
-                    ),
+                .map((item) => Semantics(
+              label: SemanticNames.getName(SemanticsType.Dot) +
+                  item.outputData +
+                  (item.press
+                      ? SemanticNames.getName(SemanticsType.Painted)
+                      : SemanticNames.getName(SemanticsType.NotPainted)),
+              button: false,
+              child: Container(
+                height: 75.0 / 330 * widget.height,//all proportions are relative to height and width of widget
+                width: 75.0 / 330 * widget.height,//proportions are made as on the layout: https://www.figma.com/file/pJE5TUjBKvdy2ZmpMnHAS4/Практика
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: AppColors.dotBoarder,
+                    width: 8,
                   ),
-                ))
+                ),
+                child: CupertinoButton(
+                  onPressed: () {
+                    if (widget.isTapped) {
+                      setState(() {
+                        item.setIsPressed(!item.press);
+                        PracticeResults.dotClick(int.parse(item.outputData));
+                      });
+                    }
+                  },
+                  borderRadius: BorderRadius.all(Radius.circular(1000),),
+                  padding: EdgeInsets.zero,
+                  color: item.p,
+                  child: Text(item.outputData,
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(fontSize: 0.18 * widget.height - 8 / 330 * widget.height, color: item.onP, fontWeight: FontWeight.bold)),
+                ),
+              ),
+            ))
                 .toList(growable: false),
           ),
         ),
