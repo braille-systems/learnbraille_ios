139011ace784f60823111b5669d0f75cc34ce7aa
Kirill Kuksenko <60854701+kirillkuks@users.noreply.github.com>
2021-04-11 13:05:55 +0300

    BRAILLE-191 Add practice results (#97)
    * BRAILLE-191 Add practice results
    
    * BRAILLE-191 Add back button functionality
    
    * BRAILLE-191 Fix syntactic error
    
    * BRAILLE-191 Change button style decloration

---
+++ braille_abc/lib/screens/results_screen.dart
@@ -0,0 +1,52 @@
+import 'package:braille_abc/components/navigation_bar_widget.dart';
+import 'package:braille_abc/components/results_info_widget.dart';
+import 'package:braille_abc/models/app_names.dart';
+import 'package:braille_abc/models/practice_model.dart';
+import 'package:braille_abc/models/screen_model.dart';
+import 'package:braille_abc/shared/non_swipeable.dart';
+import 'package:braille_abc/shared/screen_params.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class ResultsScreen extends SectionScreen {
+  final Results results;
+
+  const ResultsScreen({Key key, Widget helpPage, Widget previousPage, this.results})
+      : super(key: key, helpPage: helpPage, previousPage: previousPage);
+
+  @override
+  Widget build(BuildContext context) {
+    return nonSwipeable(
+      context,
+      CupertinoPageScaffold(
+        navigationBar: NavigationBar(
+          currentPage: this,
+          title: ScreenNames.getName(ScreenType.Results),
+        ),
+        child: SafeArea(
+          minimum: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: ScreenParams.height(4, context),
+              ),
+              Container(
+                height: ScreenParams.heightIOS14(80, context),
+                child: ResultsInfoWidget(results: ResultsInfo(results)),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                height: 48,
+                child: BackButtonWidget(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
