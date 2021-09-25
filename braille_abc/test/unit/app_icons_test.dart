d5faf4507d531da02703b64198661339608e7d69
zuevval <valera.zuev.zva@gmail.com>
2021-09-25 21:01:13 +0300

    BRAILLE-344 fix project style after flutter version upgrade

--- braille_abc/test/unit/app_icons_test.dart
+++ braille_abc/test/unit/app_icons_test.dart
@@ -3,9 +3,9 @@ import 'package:braille_abc/models/app_icons.dart';
 
 void main() {
   test('Test AppIcons has values for all icons', () {
-    for (var app_icon in AppIcons.values) {
-      var icd = AppIcon.getIcon(app_icon);
-      expect(icd, AppIcon.AppIconsMap[app_icon]);
+    for (var appIcon in AppIcons.values) {
+      var icd = AppIcon.getIcon(appIcon);
+      expect(icd, AppIcon.AppIconsMap[appIcon]);
     }
   });
 }
